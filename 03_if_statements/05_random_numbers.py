7c251726d4f859e1691dac6b96a869ae4cee4f1a
Muneeb-Ahmed <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2025-06-21 21:09:56 +0500

    Add files via upload

---
+++ 03_if_statements/05_random_numbers.py
@@ -0,0 +1,11 @@
+
+import random
+
+def main():
+    
+    for num in range(10):
+        random_value : list[int] = random.randint(1,100)
+        print(random_value)
+
+if __name__ == '__main__':
+    main()
